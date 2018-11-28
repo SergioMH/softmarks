5be3a5f069561f7787611311afdd1da46bbe2065
SergioMH <sergiomarquinah@gmail.com>
2018-11-28 10:47:15 -0600

    Add files via upload

---
+++ GreetingClient.java
@@ -0,0 +1,34 @@
+package com.softmarks.network.validation.test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class GreetingClient {
+
+   public static void main(String [] args) {
+      String serverName = "192.168.1.69";
+//      String serverName = "192.168.1.78";
+      int port = 8844;
+      try {
+         System.out.println("Client: Connecting to " + serverName + " on port " + port);
+         Socket client = new Socket(serverName, port);
+         
+         System.out.println("Just connected to " + client.getRemoteSocketAddress());
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream out = new DataOutputStream(outToServer);
+         out.writeUTF("Hello from " + client.getLocalSocketAddress());
+         
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream in = new DataInputStream(inFromServer);
+         
+         System.out.println("Server says " + in.readUTF());
+         client.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
